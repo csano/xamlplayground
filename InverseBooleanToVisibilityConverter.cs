ba290973a2c057106dc366bbce5dd252e8e3d13a
csano <csano@microsoft.com>
2017-08-24 17:17:39 -0700

    Initial commit

---
+++ InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace XamlPlayground
+{
+    public class InverseBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+            {
+                throw new Exception();
+            }
+            var converter = new BooleanToVisibilityConverter();
+            return converter.Convert(!(bool)value, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
